20d06c432363bfebc861825c0a66af0adaf5a932
Rodrigo Pirotti Alcantara <rodrigo.pirotti@live.com>
2020-10-21 11:26:04 +0200

    novas telas e imagens

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import mapMarkerImg from '../images/map-marker.svg';
+import mapIcon from '../utils/mapIcons';
+
+import "../styles/pages/orphanages-map.css"
+import api from '../services/api';
+
+interface Orphanage {
+  id: number,
+  name: string,
+  latitude: number,
+  longitude: number,
+}
+
+function OrphanegeMap() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  console.log(orphanages);
+
+  useEffect(() => {
+    api.get('orphanages').then(response => 
+      setOrphanages(response.data));
+  }, []);
+
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Escolha um organato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Rio do Sul</strong>
+          <span>Santa Catarina</span>
+        </footer>
+      </aside>
+
+      <Map 
+        center={[49.3796624,11.0209428]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url="http://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+    
+        {
+          orphanages.map(orphanage => {
+            return (
+              <Marker 
+                key={orphanage.id}
+                icon={mapIcon}
+                position={[orphanage.latitude, orphanage.longitude]}
+                >
+                  <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
+                    {orphanage.name}
+                    <Link to={`/orphanages/${orphanage.id}`}>
+                      <FiArrowRight size={28} color="#FFF" />
+                    </Link>
+
+                  </Popup>
+              </Marker>
+            )
+          })
+        }
+
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanegeMap;
